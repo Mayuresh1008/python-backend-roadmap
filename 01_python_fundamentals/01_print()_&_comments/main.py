e9a8c62cde7ac50b0cdbacf45447639ee57f4692
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-16 09:34:06 +0530

    Added main.py in print and comments folder

---
+++ 01_python_fundamentals/01_print()_&_comments/main.py
@@ -0,0 +1,34 @@
+# Comments
+
+# This is a Single-line Comment
+
+print("Hey I am a good boy \nand this viewer is also a good boy/girl.") # This is also a comment
+
+# Multiline Comment
+
+'''
+print("Hello everyone")
+print("This is a Multiline Comment")
+'''
+
+""" 
+Escape Sequence Characters: 
+To insert characters that cannot be directly used in a string, we use an escape sequence character.
+An Escape sequence character is an backlash \ followed by the character you want to insert.
+"""
+
+# Print Statement
+print("Hello World")
+
+# Multiple values in print
+# We can also add separator 
+""" 
+Separator in Print Statement:
+The separator is usually added in between the multiple values of a print statement
+
+End in Print Statement:
+It is a sort of string which gets added at the end of the print statement main content
+ex. print("Hello", end="9")
+output: Hello9
+"""
+print("Hey", 6, 7, sep="-", end="009\n")
