8abecfbad79b0c79c0e6c0c2692e4f03ffbd2855
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-17 20:39:11 +0530

    Update data_types.py file

--- 01_python_fundamentals/02_variables_&_datatypes/data_types.py
+++ 01_python_fundamentals/02_variables_&_datatypes/data_types.py
@@ -53,10 +53,22 @@ print(sentence)
 # String Methods
 
 print(first)
-print(first.lower())
-print(first.upper())
+print(first.lower()) # converts string into lowercase
+print(first.upper()) # converts string into uppercase
 print(first)
 
-print(multiline.title())
+print(multiline.title()) 
 print(multiline.replace("good", "ok"))
 print(multiline)
+
+print(len(multiline))
+multiline += "                                      "
+multiline = "                             " + multiline
+print(len(multiline))
+
+print(len(multiline.strip())) # length after removing all the white spaces.
+print(len(multiline.lstrip())) # length after removing the white spaces from left side.
+print(len(multiline.rstrip())) # length after removing the white spaces from right side.
+
+
+
