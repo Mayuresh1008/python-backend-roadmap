bc2bc4a698465babb686d0fd33c4c9e8ac77f332
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-17 21:11:58 +0530

    Update data_types.py file

--- 01_python_fundamentals/02_variables_&_datatypes/data_types.py
+++ 01_python_fundamentals/02_variables_&_datatypes/data_types.py
@@ -105,12 +105,12 @@ best_price = int(80)
 print(type(price))
 print(isinstance(best_price, int))
 
-""" 
 # float datatype
 gpa = 3.28
 y = float(1.14)
 print(type(gpa))
 
+""" 
 # complex datatype
 comp_value = 5+3j
 print(type(comp_value))
