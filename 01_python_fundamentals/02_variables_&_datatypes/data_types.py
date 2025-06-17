c49e38177804debf07a27c4aa64eaf08d2c3d4b6
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-17 20:17:19 +0530

    Update the files

---
+++ 01_python_fundamentals/02_variables_&_datatypes/data_types.py
@@ -0,0 +1,22 @@
+# String Data Type
+
+# literal assignment
+first = "Dave"
+last = "Gray"
+
+# print(type(first))
+# print(type(first) == str)
+# print(isinstance(first, str))
+
+# Constructor function to define string
+# pizza = str("Pepperoni")
+# print(type(pizza))
+# print(type(pizza) == str)
+# print(isinstance(pizza, str))
+
+# Concatenation
+fullname = first + " " + last
+print(fullname)
+
+fullname += "!"
+print(fullname)
