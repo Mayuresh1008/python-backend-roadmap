658209483b4212f2b3a2d562f552cce258e30d30
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-17 20:03:01 +0530

    Update data_types.py file

--- 01_python_fundamentals/02_variables_&_datatypes/data_types.py
+++ 01_python_fundamentals/02_variables_&_datatypes/data_types.py
@@ -4,6 +4,13 @@
 first = "Dave"
 last = "Gray"
 
-print(type(first))
-print(type(first) == str)
-print(isinstance(first, str))
+# print(type(first))
+# print(type(first) == str)
+# print(isinstance(first, str))
+
+# Constructor function to define string
+pizza = str("Pepperoni")
+print(type(pizza))
+print(type(pizza) == str)
+print(isinstance(pizza, str))
+
