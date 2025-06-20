ef88ee34c77f7fe7059c6202239d7bbd11c183c4
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-20 15:58:35 +0530

    Update operators.py

--- 01-Python-Fundamentals/06-Operators/operators.py
+++ 01-Python-Fundamentals/06-Operators/operators.py
@@ -3,3 +3,60 @@ Python Operators:
 Symbols used to perform operations on values and the variables that hold those values.
 """
 
+# # Assigment Operator
+# name = "Dave"
+# print(name)
+# number = 42
+# print(number)
+# number += 1
+# print(number)
+# number -= 1
+# print(number)
+# number *= 10
+# print(number)
+# number /= 10
+# print(number)
+# number = round(number)
+# print(number)
+
+# # Arithmetic Operator
+# print(2 + 2)
+# print(4 - 2)
+# print(2 * 2)
+# print(24 / 5)
+# print(24 // 5)
+# print(round(24 / 5))
+# print(24 % 5)
+# print(2 ** 3)
+# print(2 ** 5)
+
+# # Concatenation
+# str1 = "Dave "  + "Gray"
+# print(str1)
+
+# # Comparison Operator
+# print(42 == 41)
+# print(42 == 42)
+# print(42 != 42)
+# print(42 != 42)
+# print(10 > 5)
+# print(10 < 5)
+# print(10 >= 5)
+# print(10 <= 10)
+# print(10 <= 5)
+
+# # Logical Operators
+# x = True
+# y = False
+# z = True
+# print(not x)
+# print(not y)
+# print(x and y)
+# print(x and z)
+# print(y and z)
+# print(x or y)
+# print(x or z)
+# print(y or z)
+# print(y or x)
+
+# # Membership Operators
