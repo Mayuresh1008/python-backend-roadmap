d041807cea6153cd75731db6233a40f6dfbd32cc
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-20 10:38:25 +0530

    Update string_slicing.py file

--- 01-Python-Fundamentals/05-Strings/string_slicing.py
+++ 01-Python-Fundamentals/05-Strings/string_slicing.py
@@ -7,6 +7,7 @@ print(len1)
 
 # Slicing
 print(fruit[0:4]) # prints the string from 0 to 3 index
+# it includes 0 but not 4
 print(fruit[1:4]) # prints the string from 1 to 3 index
 print(fruit[:3])  # prints the string from 0 to 2 index even if we don't write starting index in the slice
 print(fruit[:]) # if we don't mention both the starting and ending index then it prints the whole string
@@ -22,3 +23,11 @@ it works like this _,_,_,_,_,-3,-2,-1
 print(fruit[0:-3])
 print(fruit[0:len(fruit)-2])
 print(fruit[-3:-1])
+
+# Example
+pie = "ApplePie"
+print(pie[:5])
+print(pie[5:])
+print(pie[2:6])
+print(pie[-8:])
+
