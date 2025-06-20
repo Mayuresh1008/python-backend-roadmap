4e5757499331730dccde50d1a2c54ff0fcb58e1f
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-20 16:51:24 +0530

    Update conditional_statements.py file

--- 01-Python-Fundamentals/07-Conditional-Statements/conditional_statements.py
+++ 01-Python-Fundamentals/07-Conditional-Statements/conditional_statements.py
@@ -1,7 +1,17 @@
+# If-Else
 age = int(input("Enter your age: "))
 print("Your age is:", age)
 
-if age > 18:
+if age >= 18:
     print("You can drive")
 else:
     print("You can not drive")
+
+print("")
+# Another example
+applePrice = 210
+budget = 200
+if (applePrice <= budget):
+    print("Alexa, add 1 kg Apples to the cart.")
+else:
+    print("Alexa, do not add Apples to the cart.")
