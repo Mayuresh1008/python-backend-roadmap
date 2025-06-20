f9808a6fa753c5fa3f2a6024e99f6b21680388c3
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-20 10:23:47 +0530

    Update string_slicing.py file

--- 01-Python-Fundamentals/05-Strings/string_slicing.py
+++ 01-Python-Fundamentals/05-Strings/string_slicing.py
@@ -1,2 +1,14 @@
 names = "Harry,Shubham"
-print(names[0:5])
+print(names[0:5]) # prints characters from index 0 to 4 
+
+fruit = "Mongo"
+len1 = len(fruit)
+print(len1)
+
+# Slicing
+print(fruit[0:4]) # prints the string from 0 to 3 index
+print(fruit[1:4]) # prints the string from 1 to 3 index
+print(fruit[:3])  # prints the string from 0 to 2 index even if we don't write starting index in the slice
+print(fruit[:]) # if we don't mention both the starting and ending index then it prints the whole string
+
+# Negative slicing
