d4c36dd29ad34b4c06a7c38d93f31eeff928831c
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-20 22:43:14 +0530

    Update matchcase_statements.py file

---
+++ 01-Python-Fundamentals/07-Conditional-Statements/matchcase_statements.py
@@ -0,0 +1,15 @@
+""" 
+Matchcase Statements:
+match-case statements (also called as structured pattern matching) provides a powerful alternative to long if-elif-else chains.
+It's similar to switch-case in other languages but more flexible.
+
+Basic Syntax:
+match value:
+    case pattern1:
+        # action1
+    case pattern2:
+        # action2
+    case _: # default case
+        # default action
+"""
+
