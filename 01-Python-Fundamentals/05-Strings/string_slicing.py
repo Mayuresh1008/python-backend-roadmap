4b8fa7cb8b8e6efd8568e43b262d57e2cdc2f04a
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-20 10:53:19 +0530

    Update string_slicing.py file

--- 01-Python-Fundamentals/05-Strings/string_slicing.py
+++ 01-Python-Fundamentals/05-Strings/string_slicing.py
@@ -9,6 +9,7 @@ print(len1)
 print(fruit[0:4]) # prints the string from 0 to 3 index
 # it includes 0 but not 4
 print(fruit[1:4]) # prints the string from 1 to 3 index
+# it includes 1 but not 4
 print(fruit[:3])  # prints the string from 0 to 2 index even if we don't write starting index in the slice
 print(fruit[:]) # if we don't mention both the starting and ending index then it prints the whole string
 
@@ -31,3 +32,21 @@ print(pie[5:])
 print(pie[2:6])
 print(pie[-8:])
 
+# Few more examples
+word = "Python"
+print(word[1])
+print(word[-1])
+
+text = "Hello, World!"
+print(text[0:5])
+print(text[7:])
+
+# adding third slicing will skip the number of letter
+print(text[::3]) # prints every third character skiping inbetween characters 
+# String starts with "H" further skips 2 character and prints the third one which is "l" same process will be followed further
+print(text[::1]) # print the whole string no change
+# H, e, l, l, o, ",", , W, o, r, l, d, !
+# above print will just print: Hl r!
+# the third slicing can also be used for reversing the string buy entering the negative index value and the skiping charcters process can also be followed in the reverse method also
+print(text[::-1]) # prints the whole string reverse
+print(text[::-3]) # prints every third chracter of by skiping two in reverse order
