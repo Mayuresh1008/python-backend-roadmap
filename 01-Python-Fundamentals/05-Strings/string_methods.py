c3744c4ee0f6ff791fc986b3756aeab3023ab292
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-20 11:09:57 +0530

    Update and add string_methods.py

---
+++ 01-Python-Fundamentals/05-Strings/string_methods.py
@@ -0,0 +1,22 @@
+""" 
+String are immutable we can't change the string at the permanent but their are methods to change the string temporarily.
+
+Strings Methods doesn't change the existing string they make a new string for the output which doesn't affects the original string
+"""
+
+name = "Rocky"
+print(len(name)) # displays length of the string
+print(name.upper()) # converts the string in uppercase.
+print(name.lower()) # converts the string in lowercase.
+
+greet = "    !!Hello!!!     "
+print(greet)
+print(greet.strip()) # removes white spaces from both sides
+print(greet.rstrip()) # removes white spaces frrm just right side
+print(greet.lstrip()) # removes whites spaces from left side
+
+a = "!!!HI!!!!"
+print(a)
+print(a.strip("!")) # also removes unwanted character or symbols from the string at both sides.
+print(a.rstrip("!")) # also removes unwanted character or symbols from the string at right side.
+print(a.lstrip("!")) # also removes unwanted character or symbols from the string left side.
