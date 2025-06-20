22c5d553207cdfe42bd51ff37decdeb8c0768402
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-20 10:33:52 +0530

    Update string_slicing.py file

--- 01-Python-Fundamentals/05-Strings/string_slicing.py
+++ 01-Python-Fundamentals/05-Strings/string_slicing.py
@@ -12,3 +12,13 @@ print(fruit[:3])  # prints the string from 0 to 2 index even if we don't write s
 print(fruit[:]) # if we don't mention both the starting and ending index then it prints the whole string
 
 # Negative slicing
+""" 
+Python negotiate with negative slicing like this:
+pirnt(fruit[0:len(fruit)-2])
+Means the negative value we provide while slicing it gets subtracted from the actual len of the string and we get the output.
+And we can also find the ending character buy just counting string index from the end point which is -1
+it works like this _,_,_,_,_,-3,-2,-1
+"""
+print(fruit[0:-3])
+print(fruit[0:len(fruit)-2])
+print(fruit[-3:-1])
