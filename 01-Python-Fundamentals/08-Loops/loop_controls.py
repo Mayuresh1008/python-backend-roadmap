35b0628c106c4f8488705a687a5d7b3fd51bb646
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-21 16:27:15 +0530

    added continue and pass statement example

--- 01-Python-Fundamentals/08-Loops/loop_controls.py
+++ 01-Python-Fundamentals/08-Loops/loop_controls.py
@@ -4,14 +4,21 @@
 
 for i in range(1, 10):
     if i == 5:
-        print("Stopping at 5")
-        break
+        print("Stopping at 5") 
+        break # stops at 5
     print(i)
 
 ## continue - skip the current round and go to next
 print("")
 for i in range(1, 6):
     if i == 3:
-        continue 
+        continue # Skipped 3
     print(i)
 
+## pass - ignore this for now, i will write later
+for i in range(5):
+    if i == 2:
+        pass # maybe we will write somethind here later
+    print(i)
+
+# pass is just a placeholder. 
