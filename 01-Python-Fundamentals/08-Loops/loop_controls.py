420629a07593445c480c7c3794bac7cd88a1bc0f
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-21 16:23:36 +0530

    added break statement example

--- 01-Python-Fundamentals/08-Loops/loop_controls.py
+++ 01-Python-Fundamentals/08-Loops/loop_controls.py
@@ -8,3 +8,10 @@ for i in range(1, 10):
         break
     print(i)
 
+## continue - skip the current round and go to next
+print("")
+for i in range(1, 6):
+    if i == 3:
+        continue 
+    print(i)
+
