7884bf63b898251f4c5f626daa3527867a29c0ff
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-21 16:05:21 +0530

    added another example of while loop

--- 01-Python-Fundamentals/08-Loops/while_loop.py
+++ 01-Python-Fundamentals/08-Loops/while_loop.py
@@ -3,16 +3,37 @@ While loop - When you don't know how many times you have to repeat the task
 
 - Do this again and again until a condition is false.
 """
-# While loop with increasing value
+## While loop with increasing value
 i = 0
 while (i < 3):
     print(i)
     i += 1
 
-# While loop with decreasing value
+## While loop with decreasing value
 print("")
 count = 5
 while (count > 0):
     print(count)
     count = count - 1
 
+## Else with while loop
+print("")
+x = 5
+while (x > 0):
+    print(x)
+    x = x - 1
+else:
+    print("counter is 0")
+
+# Another example
+print("")
+# Ask the user to guess the number
+secret = 7
+guess = 0
+
+while guess != secret:
+    guess = int(input("Guess the number: "))
+    if guess != secret:
+        print("Wrong! Try again.")
+    
+print("Correct guess!!!")
