6cd3c76faf9760b6e7c230f68c1176621a913f58
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-21 10:45:14 +0530

    added for loop examples in Python

---
+++ 01-Python-Fundamentals/08-Loops/for_loop.py
@@ -0,0 +1,16 @@
+""" 
+For loop - When You Know How many Times to Repeat the same task
+"""
+# Simple example
+
+name = "Abhishek"
+for i in name:
+    print(i)
+
+
+# Iterating through lists
+colors = ["Red", "Green", "Blue", "Yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
