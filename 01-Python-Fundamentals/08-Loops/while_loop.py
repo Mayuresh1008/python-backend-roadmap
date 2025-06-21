d5699784b441ae20c9601c072c7b5be4c739ef93
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-21 15:45:17 +0530

    added while loop examples with increasing and decreasing values

--- 01-Python-Fundamentals/08-Loops/while_loop.py
+++ 01-Python-Fundamentals/08-Loops/while_loop.py
@@ -3,4 +3,16 @@ While loop - When you don't know how many times you have to repeat the task
 
 - Do this again and again until a condition is false.
 """
+# While loop with increasing value
+i = 0
+while (i < 3):
+    print(i)
+    i += 1
+
+# While loop with decreasing value
+print("")
+count = 5
+while (count > 0):
+    print(count)
+    count = count - 1
 
