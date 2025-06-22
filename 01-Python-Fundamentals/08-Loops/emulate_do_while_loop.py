8003436a535c4d29e1b666bc8383a8bfdc0fa7f6
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-22 10:00:22 +0530

    Added do while loop explanation

---
+++ 01-Python-Fundamentals/08-Loops/emulate_do_while_loop.py
@@ -0,0 +1,6 @@
+""" 
+Do-while Loop:
+That loop where the code runs at least once before checking the condition
+
+Python has no built-in do while loop but we can emulate it using a while True + break
+"""
