2959e50e8e05cb168ef5c0fbe9877180a450bf7e
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-22 10:07:33 +0530

    added emulated do-while examples

--- 01-Python-Fundamentals/08-Loops/emulate_do_while_loop.py
+++ 01-Python-Fundamentals/08-Loops/emulate_do_while_loop.py
@@ -4,3 +4,34 @@ That loop where the code runs at least once before checking the condition
 
 Python has no built-in do while loop but we can emulate it using a while True + break
 """
+
+# Python Emulated do-while Example:
+# Example : Ask password until correct
+
+while True:
+    password = input ("Enter the password:")
+    
+    if password == "python123":
+        print("Access granted!!!")
+        break # condition met, exit loop
+    else:
+        print("Wrong password, Try again!!")
+    
+# Another Example : Keep adding numbers until 0 is entered
+while True:
+    num = int(input("Enter a number (0 to stop): "))
+    
+    if num == 0:
+        print("Loop ended")
+        break
+    
+    print(f"You entered: {num}")
+
+""" 
+Trick Template We can use 
+while True:
+    # your code that should run at least once
+    
+    if not condition:
+        break
+"""
