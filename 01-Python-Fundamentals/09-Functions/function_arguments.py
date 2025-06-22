01bb57b02ade89df022a7cf5af00f0c531e42cd7
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-22 17:34:11 +0530

    added examples of default arguments

--- 01-Python-Fundamentals/09-Functions/function_arguments.py
+++ 01-Python-Fundamentals/09-Functions/function_arguments.py
@@ -8,8 +8,18 @@ There are four types of arguments that we can provide in a function:
 > Required Arguments
 """
 
+## Default Arguments
 def average(a=9, b=1): # if we dont provide any value then this values are used by the function for execution
     print("The average is ", (a + b/ 2))
     
 # average(4, 6)
 average(1, 5) # this values will replace the default values
+average(5) # a=5,b will be the default value 1 
+average(b=9) # here b=9, a will be the default value 9
+
+## Another example
+def name(fname, mname = "Jhon", lname = "Whatson"):
+    print("Hello, ", fname, mname, lname)
+
+name("Amy")
+name("Mayuresh", "Umesh", "Patil")
