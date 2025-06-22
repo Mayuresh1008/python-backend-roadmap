07d515ed1b9799204718fbe7294ae0a86052f150
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-22 22:01:22 +0530

    added examples of function with no parameters and no return statement

--- 01-Python-Fundamentals/09-Functions/function_types.py
+++ 01-Python-Fundamentals/09-Functions/function_types.py
@@ -14,3 +14,23 @@ There are 4 types of functions in Python:
 >> Function takes inputs, returns the result and performs the calculation and logic.
 """
 
+## 1. Functions with No Parameters and No Return
+
+# Example 1:
+def welcome():
+    print("Welcome to the Python course!")
+
+welcome() # just shows the message no input needed
+
+# Example 2:
+def show_date():
+    print("Today is Saturday.")
+
+show_date() # just shows message, can't reuse or store anything
+
+# Example 3:
+def say_hello():
+    for i in range(3):
+        print("Hello!")
+
+say_hello()
