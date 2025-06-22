85e55b9d556cdf428f391397759810eef45d8644
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-22 22:41:03 +0530

    added explanation of built-in functions

---
+++ 01-Python-Fundamentals/09-Functions/built_in_functions.py
@@ -0,0 +1,15 @@
+""" 
+Built-in functions are predefined functions that come with python
+you don't need to install or import anything
+
+They help us to:
+Take input - input()
+print output - print()
+do math - abs(), round(), pow(x, y), max(), min(), sum()
+work with data - len(), sorted(), reversed(), enumerate(), zip(), strip(), rstrip(), lstrip()
+debug your code - id(), help(), dir(), eval()
+type conversion - int(), float(), str(), bool(), list(), tuple()
+type checking - type(), isinstance()
+
+"""
+
