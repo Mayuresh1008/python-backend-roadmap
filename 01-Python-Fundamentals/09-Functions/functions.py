29661c03a05ca782edd0467a28f3b0396f1918da
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-22 16:43:01 +0530

    added explanation of functions

--- 01-Python-Fundamentals/09-Functions/functions.py
+++ 01-Python-Fundamentals/09-Functions/functions.py
@@ -1,4 +1,11 @@
 """ 
 Functions in Python - A function is a block of code that does something specific, and you can reuse it anywhere in your program
+
+Types of functions:
+1. Built-in functions
+2. User-defined functions
 """
 
+
+
+ 
