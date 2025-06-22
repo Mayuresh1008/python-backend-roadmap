0aa23c39508aa0bfb5d97eccc598c91e5c9d35c5
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-22 21:56:00 +0530

    added function types explanation

--- 01-Python-Fundamentals/09-Functions/function_types.py
+++ 01-Python-Fundamentals/09-Functions/function_types.py
@@ -1,7 +1,16 @@
 """ 
-There are 4 types of functions in Python
-1. No parameter, no return 
-2. With parameter, no return 
-3. No parameter, with return 
-4. With parameter and return
+There are 4 types of functions in Python:
+
+1. No parameter, no return - Function with No Parameters and No Return:
+>> Function doesn't take any input , doesn't give any result back and just performs a task like printing something.
+
+2. With parameter, no return - Function with Parameters and No Return:
+>> Function takes input (parameters), doesn't return anything and performs a task
+
+3. No parameter, with return - Function with No Parameters but with Return
+>> Function doesn't take any input, but gives back a value using return
+
+4. With parameter and return - Function with Parameters and Return
+>> Function takes inputs, returns the result and performs the calculation and logic.
 """
+
