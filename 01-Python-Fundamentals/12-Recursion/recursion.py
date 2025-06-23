50a5d58bc968bdf1dedbf31f70e67de896404463
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-23 16:50:32 +0530

    updated the files

---
+++ 01-Python-Fundamentals/12-Recursion/recursion.py
@@ -0,0 +1,11 @@
+""" 
+Recursion is when a function calls itself to solve a smaller version of the same position
+
+Basic Structure (Syntax):
+def recursive_function(): # Base case
+    if condition_to_stop:
+        return something
+    else:
+        return recursive_function() # Rwcursive call
+"""
+
