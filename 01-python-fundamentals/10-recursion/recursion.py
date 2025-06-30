4899e93bbf132725fc68c47b7bf90a74d6feccdb
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-06-30 17:03:07 +0530

    added explanation and simple example of recursion

--- 01-python-fundamentals/10-recursion/recursion.py
+++ 01-python-fundamentals/10-recursion/recursion.py
@@ -1,5 +1,6 @@
-""" 
-Recursion is when a function calls itself to solve a smaller version of the same position
+"""
+Recursion is when a function calls itself to solve a smaller version of the same position.
+Always has a base to stop the recursion
 
 Basic Structure (Syntax):
 def recursive_function(): # Base case
@@ -7,17 +8,20 @@ def recursive_function(): # Base case
         return something
     else:
         return recursive_function() # Recursive call
+
 """
 
 # factorial(7) - 7*6*5*4*3*2*1
 # factorial(n) = n * factorial(n-1)
 # Calculating factorial using recursion function
 
+
 def factorial(num):
-    if (num == 0 or num == 1):
+    if num == 0 or num == 1:
         return 1
     else:
         return num * factorial(num - 1)
+
 
 print(factorial(3))
 print(factorial(4))
