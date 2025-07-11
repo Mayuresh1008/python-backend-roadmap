49eedd6f58e73f5b9f41fa0c16b8b85b8e95e87b
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-07-11 19:34:12 +0530

    added modules definition and types

---
+++ 01-python-fundamentals/11-modules/modules.py
@@ -0,0 +1,11 @@
+"""
+Modules:
+A module is just a Python file that contains functions, variables, or classes - which you can reuse in other Python Files.
+
+It Helps in breaking code into smaller parts so your programs become clean and organized.
+
+Types of Modules:
+1) Built-in Modules - Already available in Python
+2) Custom Modules - Created by user
+3) External Modules - installed using pip
+"""
