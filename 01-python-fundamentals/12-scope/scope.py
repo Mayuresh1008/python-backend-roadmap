0db3c1702ff63c5826f6e928176b48d9e7b99f7e
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-07-21 23:07:42 +0530

    added another example of scope

--- 01-python-fundamentals/12-scope/scope.py
+++ 01-python-fundamentals/12-scope/scope.py
@@ -39,3 +39,21 @@ x = 99
 
 # func3()
 # print(x)
+
+## Scope Example
+x = "global"
+
+
+def outer():
+    x = "enclosing"
+
+    def inner():
+        x = "local"
+        print("Inner:", x)
+
+    inner()
+    print("Outer:", x)
+
+
+outer()
+print("Global:", x)
