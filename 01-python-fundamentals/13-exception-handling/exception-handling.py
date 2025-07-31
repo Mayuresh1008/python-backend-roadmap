a4e2ab92b44184f9d1888a1f228cc4bb8821b2c2
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-07-31 15:29:57 +0530

    added explantion of Exception and Custom Exception block

--- 01-python-fundamentals/13-exception-handling/exception-handling.py
+++ 01-python-fundamentals/13-exception-handling/exception-handling.py
@@ -1,23 +1,37 @@
 """
-try : Block where you write code that may cause an error(exception).
+> try : Block where you write code that may cause an error(exception).
 Python "tries" to run this block
-
-except : Block that catching and handles errors raised in the try block.
-You can specify error types (like ZeroDivisionError, ValueError) or catch all.
-
-else : Runs only if no error occurs in the try block. Often used to execute "safe" code.
-
-finally : Runs no matter what - whether an error happened or not. Good for cleanup actions like closing files, ending connections.
-
-raise : You can raise to manually trigger an error. Useful when you want to enforce rules in your code.
-"""
 
 try:
     x = 1 / 0
+
+> except : Block that catching and handles errors raised in the try block.
+You can specify error types (like ZeroDivisionError, ValueError) or catch all.
+
 except ZeroDivisionError:
     print("Can't divide by zero.")
+
+> else : Runs only if no error occurs in the try block. Often used to execute "safe" code.
+
 else:
     print("Everything went fine.")
+
+> finally : Runs no matter what - whether an error happened or not. Good for cleanup actions like closing files, ending connections.
+
 finally:
     print("This block always runs.")
-# raise ValueError("incorrect value")
+
+> raise : You can raise to manually trigger an error. Useful when you want to enforce rules in your code.
+
+raise ValueError("incorrect value")
+
+> Exception : Base class for all the exceptions in Python.
+
+except Exception as e:
+    print(e)
+
+> Custom Exception : You can create your own exception type by subclassing Exception.
+
+class NegativeNumberError(Exception):
+    pass
+"""
