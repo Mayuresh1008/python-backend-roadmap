e879c190ce1bdedd5dafab3b93797bd3d907e937
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-07-31 16:31:42 +0530

    added notes of exception handling

--- 01-python-fundamentals/13-exception-handling/exception-handling.py
+++ 01-python-fundamentals/13-exception-handling/exception-handling.py
@@ -1,48 +1,3 @@
-"""
-> try : Block where you write code that may cause an error(exception).
-Python "tries" to run this block
-
-try:
-    x = 1 / 0
-
-> except : Block that catching and handles errors raised in the try block.
-You can specify error types (like ZeroDivisionError, ValueError) or catch all.
-
-except ZeroDivisionError:
-    print("Can't divide by zero.")
-
-> else : Runs only if no error occurs in the try block. Often used to execute "safe" code.
-
-else:
-    print("Everything went fine.")
-
-> finally : Runs no matter what - whether an error happened or not. Good for cleanup actions like closing files, ending connections.
-
-finally:
-    print("This block always runs.")
-
-> raise : You can raise to manually trigger an error. Useful when you want to enforce rules in your code.
-
-raise ValueError("incorrect value")
-
-> Exception : Base class for all the exceptions in Python.
-
-except Exception as e:
-    print(e)
-
-> Custom Exception : You can create your own exception type by subclassing Exception.
-
-class NegativeNumberError(Exception):
-    pass
-
-> ZeroDivisionError, ValueError, etc. : Built-in error classes in Python that represent specific problems:
-ZeroDivisionError --> Division by zero
-ValueError --> Invalid type conversion (e.g., int("abc"))
-TypeError --> Invalid operation between mismatched types
-IndexError --> Index out of range for lists
-KeyError --> Missing key in dictionary
-"""
-
 # Example 1 : Division with Multiple Exceptions
 
 try:
@@ -109,3 +64,18 @@ except IndexError:
     print("That index is out of range.")
 except Exception as e:
     print("Some other error occured:", e)
+
+print("")
+# Example 5: File Reader with finally
+try:
+    file = open("notes.txt", "r")
+    content = file.read()
+    print("File Content:\n", content)
+except FileNotFoundError:
+    print("File not found")
+finally:
+    try:
+        file.close()
+    except:
+        pass
+    print("File closed.")
