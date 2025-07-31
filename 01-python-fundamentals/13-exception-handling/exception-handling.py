a51b3b5d88c0fc81b730143a19cdbea796a3f638
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-07-31 15:19:38 +0530

    added else block explanation and example in the file

--- 01-python-fundamentals/13-exception-handling/exception-handling.py
+++ 01-python-fundamentals/13-exception-handling/exception-handling.py
@@ -4,9 +4,13 @@ Python "tries" to run this block
 
 except : Block that catching and handles errors raised in the try block.
 You can specify error types (like ZeroDivisionError, ValueError) or catch all.
+
+else : Runs only if no error occurs in the try block. Often used to execute "safe" code.
 """
 
 try:
     x = 1 / 0
 except ZeroDivisionError:
     print("Can't divide by zero.")
+else:
+    print("Everything went fine.")
