6e45cd86eb920b504afa637556401b386d0a3474
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-07-31 18:57:23 +0530

    added learnings related to creating lists, indexing & slicing, modifying, list methods, list comprehension

--- 02-python-datastructures/01-lists/lists.py
+++ 02-python-datastructures/01-lists/lists.py
@@ -1,1 +1,54 @@
-# This this the file of lists
+## Lists are ordered, mutable, and can store elements of different data type.
+
+# Creating Lists
+fruits = ["apple", "banana", "cherry"]
+numbers = [1, 2, 3, 4, 5]
+mixed = [1, "hello", 3.5, True]
+
+print(fruits)
+print(numbers)
+print(mixed)
+
+# Indexing & Slicing
+print(fruits[0])
+print(fruits[-1])
+print(fruits[1:])
+
+# Modifying Lists (Mutable)
+fruits[1] = "mango"  # Replaces "banana" with "mango"
+print(fruits)
+
+# Common lists Methods
+fruits.append("orange")
+print(fruits)
+
+fruits.insert(1, "grapes")
+print(fruits)
+
+fruits.remove("cherry")
+print(fruits)
+
+last = fruits.pop()
+print(last)
+print(fruits)
+
+count = fruits.count("apple")
+print(count)
+
+length = len(fruits)
+print(length)
+
+# fruits.sort()
+fruits.sort(reverse=True)
+print(fruits)
+
+fruits.reverse()
+print(fruits)
+
+# Looping through lists
+for fruit in fruits:
+    print(fruit)
+
+# List Comprehension (Mini Loop)
+squares = [x * x for x in range(1, 6)]
+print(squares)
