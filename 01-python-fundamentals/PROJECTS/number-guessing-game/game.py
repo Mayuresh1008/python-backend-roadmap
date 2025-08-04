655544fe4908ce7ac4e74797300324f42d8bf174
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-08-04 22:33:53 +0530

    made number guessing game

--- 01-python-fundamentals/PROJECTS/number-guessing-game/game.py
+++ 01-python-fundamentals/PROJECTS/number-guessing-game/game.py
@@ -1,1 +1,20 @@
 import random
+
+secret_number = random.randint(1, 100)
+
+correct = False
+attempts = 0
+
+while not correct:
+    guess = int(input("Guess the number:"))
+    attempts += 1
+
+    if guess == secret_number:
+        print(f"You guessed the correct number {secret_number} in {attempts} attempts.")
+        correct = True
+    elif guess > secret_number:
+        print("Too High")
+    elif guess < secret_number:
+        print("Too Low")
+    else:
+        print("Invalid input")
