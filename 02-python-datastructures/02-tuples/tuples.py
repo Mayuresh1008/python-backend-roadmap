b5e9304e1cdfa947f0c891660ff22ea67f3c8664
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-08-22 21:02:03 +0530

    added examples of creating tuples

--- 02-python-datastructures/02-tuples/tuples.py
+++ 02-python-datastructures/02-tuples/tuples.py
@@ -1,1 +1,24 @@
-# this is a file of tuples
+"""
+A tuple is like a list, but immutable (you cannot modify it after creation).
+Think of it as a read-only list (It is used when you don't want data to change).
+"""
+
+# Creating Tuples
+# normal
+t1 = (1, 2, 3)
+print(t1)
+
+# parenthesis are optional
+t2 = 1, 2, 3
+print(t2)
+
+# single-element tuple
+single = (42,)
+# not_tuple = (42)
+
+print(single)
+# print(not_tuple)
+
+# empty tuple
+empty = ()
+print(empty)
