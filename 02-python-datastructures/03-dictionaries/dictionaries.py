5ea6ef02face58a6448c5f36926beed1c4cc85f4
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-08-23 18:18:40 +0530

    added examples of creating dictionaries

---
+++ 02-python-datastructures/03-dictionaries/dictionaries.py
@@ -0,0 +1,26 @@
+"""
+A dictionary is a collection of key-value pairs.
+Keys must be unique and immutable.
+"""
+
+## Creating Dictionaries
+
+# Empty Dict
+d1 = {}
+print(d1)
+
+# With values
+d2 = {"name": "Mayuresh", "age": 22, "role": "Dev"}
+print(d2)
+
+# Using dict()
+d3 = dict(name="Raj", age=21)
+print(d3)
+
+# Nested dict
+d4 = {
+    "student1": {"name": "Raj", "age": 20},
+    "student2": {"name": "Mayuresh", "age": 22},
+}
+
+print(d4)
