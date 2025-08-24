3937dbbc6c41f8f0c0b6733cdb2baccc17f7b180
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-08-24 20:13:36 +0530

    added examples of set operations

--- 02-python-datastructures/04-sets/sets.py
+++ 02-python-datastructures/04-sets/sets.py
@@ -32,3 +32,12 @@ print(s)
 
 s.clear()
 print(s)
+
+# Set Operations
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+
+print(a | b)  # union
+print(a & b)  # intersection
+print(a - b)  # difference
+print(a ^ b)  # symmetric difference
