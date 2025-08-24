0315259f0177dc51dfefa2d642311c5afa5b80ac
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-08-24 20:06:42 +0530

    added examples of creating sets

--- 02-python-datastructures/04-sets/sets.py
+++ 02-python-datastructures/04-sets/sets.py
@@ -2,3 +2,13 @@
 A set is an unordered collection of unique elements.
 Think of it as a mathematical set (no duplicates)
 """
+
+# Creating Sets
+s1 = {1, 2, 3, 4}
+print(s1)
+
+s2 = set([3, 4, 5, 6])
+print(s2)
+
+empty = set()
+print(empty)
