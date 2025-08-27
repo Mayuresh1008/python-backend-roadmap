77f632b85737b1afa8cdcc2dfebef5f5667c61c8
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-08-27 19:57:15 +0530

    added defination and operation types of stack

---
+++ 02-python-datastructures/05-stacks/stacks.py
@@ -0,0 +1,11 @@
+"""
+STACKS (LIFO - Last in, First Out)
+What is Stack?
+- a linear data structure where the last element added (pushed) is the first to be removed (popped)
+- Example : Stack of books - you can add/remove from the top.
+Operations:
+1) Push -> Add an element to the top.
+2) Pop -> Remove the top element
+3) Peek/Top -> See the top element without removing.
+4) isEmpty -> Check if stack is empty.
+"""
