cf261ec447c22d8abbe2cd50a838e70e48e34af9
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-08-28 18:25:17 +0530

    added defination and operations list of queues

---
+++ 02-python-datastructures/06-queues/queues.py
@@ -0,0 +1,12 @@
+"""
+QUEUES (FIFO - First In, First Out)
+What is a Queue?
+- A linear data structure where the first element added(enqueued) is the first to be removed(dequeued).
+- Example : Line at a ticket counter -> the person who comes first, gets served first.
+
+Operations:
+1) Enqueue -> Add element at the rear.
+2) Dequeue -> Remove element from the front.
+3) Peek/Front -> See the first element without removing.
+4) IsEmpty -> Check if queue is empty.
+"""
